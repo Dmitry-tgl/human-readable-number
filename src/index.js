159fc0171892e35202f4df7b51b32c62724fefdf
Dmitry-tgl <arzh86@mail.ru>
2021-03-14 21:42:23 +0400

    fix: solve the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,53 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    let numbersFromTenToNineteen = [
+        "zero",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+    ];
+    numbersTens = [
+        0,
+        "ten",
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+    ];
+    if (number < numbersFromTenToNineteen.length) {
+        return numbersFromTenToNineteen[number];
+    }
+    let res = "";
+    let hundreds = parseInt(number / 100);
+    let decimals = number % 100;
+    hundreds > 0 ? (res = numbersFromTenToNineteen[hundreds] + " hundred") : {};
+    if (decimals >= 20) {
+        res = res + " " + numbersTens[parseInt(decimals / 10)];
+        decimals % 10 > 0
+            ? (res = res + " " + numbersFromTenToNineteen[decimals % 10])
+            : {};
+    } else if (decimals > 0) {
+        res = res + " " + numbersFromTenToNineteen[decimals];
+    }
+    res.startsWith(" ") ? (res = res.slice(1)) : {};
+    return res;
+};
